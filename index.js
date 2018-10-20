16317030cd8cf7505b49fb7385c973c3a1d32a7a
RafM <rkjmosiolek@gmail.com>
2018-10-20 12:34:51 +0100

    build basic authentication to obtain OAth token, register new access token; store it in configstore

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files.js');
 const inquirer = require('./lib/inquirer');
+const github = require('./lib/github');
 
 clear();
 console.log(
@@ -17,8 +18,12 @@ if (files.directoryExists('.git')) {
 }
 
 const executeTheInquirer = async () => {
-    const credentials = await inquirer.askGithubCredentials();
-    console.log(credentials);
+    let token = github.getStoredGitHubToken();
+    if (!token) {
+        await github.setGithubCredentials();
+        token = await github.registerNewToken();
+    }
+    console.log(token);
 }
 
 executeTheInquirer();
